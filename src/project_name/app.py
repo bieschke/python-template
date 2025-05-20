086ab61542ea60cf193d322aa1b774a18ad68b2b
Eric Bieschke <everyman@gmail.com>
2025-05-20 03:24:04 -0700

    test: restore docstrings

--- src/project_name/app.py
+++ src/project_name/app.py
@@ -2,5 +2,13 @@
 
 
 def hello(name: str) -> str:
-    """Return a friendly greeting."""
+    """Return a friendly greeting.
+
+    Args:
+        name: The name to greet.
+
+    Returns:
+        The greeting message.
+
+    """
     return f"Hello, {name}!"
